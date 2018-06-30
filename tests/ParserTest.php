3c0cc872ed12d98ab8539a6f8fc8eda2122c1957
Yan Marques <marques_yan@outlook.com>
2018-07-01 09:58:07 +1200

    test - Add unit test for Parser

---
+++ tests/ParserTest.php
@@ -0,0 +1,222 @@
+<?php
+
+namespace Tests\Lincable;
+
+use Lincable\Parser;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Container\Container;
+use Lincable\Contracts\Formatters\Formatter;
+use Lincable\Exceptions\NotDynamicOptionException;
+
+class ParserTest extends TestCase
+{
+    private $parser;
+
+    public function setUp()
+    {
+        $this->parser = new DotParser; 
+        $this->parser->setContainer(new Container);
+    }
+
+    /**
+     * Should add a list of formatters.
+     * 
+     * @return void
+     */
+    public function testThatAddFormattersAddAnArray()
+    {
+        $expected = [
+            new FooFormatter,
+            BarFormatter::class
+        ];
+    
+        $this->parser->addFormatters($expected);
+        $this->assertEquals(
+            $expected,
+            $this->parser->getFormatters()
+        );
+    }
+
+    /**
+     * Should find the formatter by its name.
+     * 
+     * @return void
+     */
+    public function testThatFindFormatterReturnsTheClassName()
+    {
+        $expected = ALongNameFormatter::class;
+        $this->parser->addFormatter($expected);
+        $result = $this->parser->findFormatter('aLongName');
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Should return the year value.
+     * 
+     * @return void
+     */
+    public function testThatParseReturnsFoo()
+    {
+        $expected = 'foo';
+        $this->parser->addFormatter(new FooFormatter);
+        $result = $this->parser->parse('id.foo');
+        $this->assertEquals(
+            $expected, $result
+        );
+    }
+
+    /**
+     * Should return an array with the expected matches.
+     * 
+     * @return void
+     */
+    public function testThatgetMatchesReturnsArrayWithMatches()
+    {
+        $expected = ['id', 'bar'];
+        $this->parser->addFormatter(BarFormatter::class);
+        $result = $this->parser->getMatches('id.bar');
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Should return false for parse because option is not dynamic.
+     * 
+     * @return void
+     */
+    public function testThatParseThrowsNotDynamicOptionException()
+    {
+        $this->expectException(NotDynamicOptionException::class);
+        $this->parser->parse('useless');
+    }
+
+    /**
+     * Should resolves the ArgumentFormatter instance with container.
+     * 
+     * @return void
+     */
+    public function testThatFindFormatterResolvesTheArgumentFormatter()
+    {
+        $expected = 'foo';
+        $container = new Container;
+
+        // Bind the Param class instance to container for further 
+        // ArgumentFormatter class resolution.
+        $container->instance(Param::class, new Param($expected));
+        
+        // Change the parser container class.
+        $this->parser->setContainer($container);
+
+        // Add the formatter class name. The class constructor
+        // expects and Param class as argument, once we have bound
+        // a Param instance on container, the dependency injection should
+        // be performed.
+        $this->parser->addFormatter(ArgumentFormatter::class);
+
+        $result = $this->parser->parse('id.argument');
+
+        $this->assertEquals($expected, $result);
+    }
+}
+
+class DotParser extends Parser
+{
+    /**
+     * Create a new class instance.
+     * 
+     * @param  Illuminate\Contracts\Container\Container $app
+     * @param  mixed $formatters
+     * @return void
+     */
+    public function __construct(Container $app = null, $formatters = [])
+    {
+        $this->app = $app;
+        $this->formatters = collect($formatters);
+    }
+
+     /**
+     * @inheritdoc
+     */
+    protected function parseMatches(array $matches)
+    {
+        return $this->callFormatter(last($matches));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getDynamicPattern() 
+    {
+        return '/^([a-zA-Z_]+)\.([a-zA-Z_]+)$/';
+    }
+}
+
+class FooFormatter implements Formatter
+{
+    public function format($value = null)
+    {
+        return 'foo';
+    }
+}
+
+class BarFormatter implements Formatter
+{
+    public function format($value = null)
+    {
+        return 'bar';
+    }
+}
+
+class ALongNameFormatter implements Formatter
+{
+    public function format($value = null)
+    {
+        return 'A long text returned here';
+    }
+}
+
+class ArgumentFormatter implements Formatter
+{
+    private $param;
+
+    /**
+     * Create a new class instance.
+     * 
+     * @param  \Tests\Param $param
+     * @return void
+     */
+    public function __construct(Param $param)
+    {
+        $this->param = $param;
+    }
+
+    public function format($value = null)
+    {
+        return $this->param->getValue();
+    }
+}
+
+class Param
+{
+    private $value;
+
+    /**
+     * Create a new class instance. 
+     *
+     * @param  mixed $value
+     * @return void
+     */
+    public function __construct($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * Return the value parameter.
+     * 
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;        
+    }
+}
