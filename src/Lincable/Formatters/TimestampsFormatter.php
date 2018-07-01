5a9528e2d99158760f20e2def34a34028643104a
Yan Marques <marques_yan@outlook.com>
2018-07-01 15:50:32 +1200

    feat(TimestampsFormatter) - Add a hashed timestamps formatter

---
+++ src/Lincable/Formatters/TimestampsFormatter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Lincable\Formatters;
+
+use Carbon\Carbon;
+use Lincable\Contracts\Formatters\Formatter;
+
+/**
+ * This class formats a hash of the current timestamps. 
+ */
+class TimestampsFormatter implements Formatter
+{
+    /**
+     * @inheritdoc
+     */
+    public function format($value = null)
+    {
+        return sha1(Carbon::now()->timestamp);
+    }
+}
