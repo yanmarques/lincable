fc39559bdf37026dc602a786f7d68a315518b667
Yan Marques <marques_yan@outlook.com>
2018-07-01 15:11:37 +1200

    refactor(MonthFormatter) - Add Formatter interface and rewrite format method

--- src/Lincable/Formatters/MonthFormatter.php
+++ src/Lincable/Formatters/MonthFormatter.php
@@ -8,13 +8,13 @@ use Lincable\Contracts\Formatters\Formatter;
 /**
  * This class formats the current month. 
  */
-class MonthFormatter
+class MonthFormatter implements Formatter
 {
     /**
      * @inheritdoc
      */
     public function format($value = null)
     {
-        return Carbon::month;
+        return Carbon::now()->month;
     }
 }
