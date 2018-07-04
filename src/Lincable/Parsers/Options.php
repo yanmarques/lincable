b8c6f521ec2cca27ec21074a75ef9216607c9dc2
Yan Marques <marques_yan@outlook.com>
2018-07-04 13:22:27 +1200

    feat(Options) - Add ParameterInterface implementation

---
+++ src/Lincable/Parsers/Options.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Lincable\Parsers;
+
+use Lincable\Contracts\Parsers\ParameterInterface;
+
+class Options implements ParameterInterface
+{
+    /**
+     * The options value name.
+     * 
+     * @var string
+     */
+    protected $value;
+
+    /**
+     * The options parameters.
+     * 
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct(string $value, $params = null)
+    {
+        $this->value = $value;
+        $this->params = array_wrap($params);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getParams(): array
+    {
+       return $this->params; 
+    }
+}
