3874a46c41ec6451d0e84ff3053e647df9d15a5f
Yan Marques <marques_yan@outlook.com>
2018-07-04 14:10:18 +1200

    refactor(BuildClassnames) - Rewrite trait to get class basename to camel

--- src/Lincable/Concerns/BuildClassnames.php
+++ src/Lincable/Concerns/BuildClassnames.php
@@ -7,32 +7,29 @@ use Illuminate\Support\Str;
 trait BuildClassnames
 {
     /**
-     * Return the first item where the class matches the 
-     * formated class name. 
+     * Return the basename of class do camel case removing 
+     * unecessary suffixes. 
      *
-     * @param  string $name
-     * @param  array  $classes
+     * @param  mixed  $class
      * @param  string $suffix
      * @return mixed
      */
-    protected function firstClassName(string $name, array $classes, string $suffix)
+    protected function nameFromClass($class, string $suffix = null)
     {
-        return array_first($classes, function ($class) use ($name, $suffix) {
-            $class = $this->nameToCamelCase($class);            
+        $name = $this->classToCamelCase($class);            
             
-            return Str::endsWith($class, $suffix)
-                ? $name === substr($class, 0, strlen($suffix) * -1)
-                : $name === $class;
-        });
+        return Str::endsWith($name, $suffix)
+            ? substr($name, 0, strlen($suffix) * -1)
+            : $name;
     }
 
     /**
-     * Return the class name to camel case.
+     * Return the class basename to camel case.
      * 
      * @param  mixed $class
      * @return string
      */
-    protected function nameToCamelCase($class)
+    protected function classToCamelCase($class)
     {
         return Str::camel(class_basename($class));
     }
