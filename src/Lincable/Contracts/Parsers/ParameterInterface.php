1816664eed28782e517cb3a0fc307f972ba1f799
Yan Marques <marques_yan@outlook.com>
2018-07-04 13:08:44 +1200

    feat(ParameterInterface) - Add a interface for parameter options

---
+++ src/Lincable/Contracts/Parsers/ParameterInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Lincable\Contracts\Parsers;
+
+interface ParameterInterface
+{
+    /**
+     * Create a new parameter instance.
+     * 
+     * @param  string $value
+     * @param  mixed|null  $params
+     * @return void
+     */
+    public function __construct(string $value, $params = null);
+
+    /**
+     * Get the parameter value.
+     * 
+     * @return string
+     */
+    public function getValue(): string;
+
+    /**
+     * Get all parameters.
+     * 
+     * @return array
+     */
+    public function getParams(): array;
+}
