e6eb0255555a90cdca58c10e2a6d3866d8ca7b5a
= <=>
2018-07-05 09:59:13 -0300

    chore(Compiler) - Add parser getter/setter for compiler interface

--- src/Lincable/Contracts/Compilers/Compiler.php
+++ src/Lincable/Contracts/Compilers/Compiler.php
@@ -2,6 +2,8 @@
 
 namespace Lincable\Contracts\Compilers;
 
+use Lincable\Parsers\Parser;
+
 interface Compiler
 {
     /**
@@ -19,4 +21,27 @@ interface Compiler
      * @return array
      */
     public function parseUrlFragments(string $url): array;
+
+    /**
+     * Build an url from array fragments.
+     *
+     * @param  array $fragments
+     * @return string
+     */
+    public function buildUrlFragments(array $fragments): string;
+
+    /**
+     * Set the parser used on compiler.
+     *
+     * @param  \Lincable\Parsers\Parser $parser
+     * @return void
+     */
+    public function setParser(Parser $parser);
+
+    /**
+     * Get the current parser used on compiler.
+     *
+     * @return \Lincable\Parsers\Parser
+     */
+    public function getParser(): Parser;
 }
