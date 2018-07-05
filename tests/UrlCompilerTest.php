62f4a4b8711993a8a4bcbda6c540d4f732a12799
= <=>
2018-07-05 10:34:32 -0300

    test(UrlCompiler) - Add tests for new methods

--- tests/UrlCompilerTest.php
+++ tests/UrlCompilerTest.php
@@ -2,10 +2,14 @@
 
 namespace Tests\Lincable;
 
+use LogicException;
 use Lincable\UrlCompiler;
+use Lincable\Parsers\Parser;
+use Lincable\Parsers\Options;
 use PHPUnit\Framework\TestCase;
 use Lincable\Parsers\ColonParser;
 use Illuminate\Container\Container;
+use Lincable\Contracts\Parsers\ParameterInterface;
 
 class UrlCompilerTest extends TestCase
 {
@@ -24,20 +28,95 @@ class UrlCompilerTest extends TestCase
      */
     public function testThatCompileReturnTheArrayWithDynamicParameters()
     {
-        $expected = ['foo', 'bar'];
-        $result = $this->compiler->compile('example/:foo/test/:bar');
+        $expected = [['foo'], ['bar']];
+        $result = $this->compiler->parseDynamics('example/:foo/test/:bar');
         $this->assertEquals($expected, $result);
     }
 
     /**
-     * Should return an empty array.
+     * Should return the url compiled with the dynamic parametters formatted.
      *
      * @return void
      */
-    public function testThatCompileReturnAnEmptyArray()
+    public function testThatCompileReturnTheUrlCompiled()
     {
-        $expected = [];
-        $result = $this->compiler->compile('foo/bar/baz');
+        $expected = 'foo/bar';
+        $this->compiler->getParser()->addFormatter(Bar::class);
+        $result = $this->compiler->compile('foo/:bar');
         $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Should expect and LogicException because no formatter was found.
+     *
+     * @return void
+     */
+    public function testThatCompileReturn()
+    {
+        $this->expectException(LogicException::class);
+        $this->compiler->compile('foo/:bar');
+    }
+
+    /**
+     * Should return the same url passed due to any formatter.
+     *
+     * @return void
+     */
+    public function testThatCompileReturnTheExactSameUrlPassed()
+    {
+        $expected = 'foo/bar/@baz';
+        $result = $this->compiler->compile($expected);
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * Should change the parser and compile the url for new parser.
+     *
+     * @return void
+     */
+    public function testThatSetParserChangesTheParserUsedOnCompiler()
+    {
+        $parser = new FooParser;
+        
+        $parser->addFormatter(function () {
+            return 'baz';
+        }, 'bar');
+
+        $this->compiler->setParser($parser);
+        $expected = 'test/baz';
+        $result = $this->compiler->compile('test/foo@bar');
+        $this->assertEquals($expected, $result);
+    }
+}
+
+class Bar
+{
+    public function format()
+    {
+        return 'bar';
+    }
+}
+
+class FooParser extends Parser
+{
+    public function __construct()
+    {
+        $this->boot(new Container);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function parseMatches(array $matches): ParameterInterface
+    {
+        return new Options(head($matches));
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getDynamicPattern(): string
+    {
+        return '/^foo@([a-zA-Z]+)$/';
     }
 }
