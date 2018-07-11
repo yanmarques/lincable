a23746dff29c25a190eaadc66cf009ec24af3d79
Yan Marques <marques_yan@outlook.com>
2018-07-11 14:44:01 -0300

    test - Add test for the service provider

---
+++ tests/Providers/MediaManagerServiceProviderTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Lincable;
+
+use Closure;
+use Lincable\MediaManager;
+use Tests\Lincable\TestCase;
+use Illuminate\Container\Container;
+use Lincable\Eloquent\Events\UploadSuccess;
+use Lincable\Eloquent\Events\UploadFailure;
+use Lincable\Providers\MediaManagerServiceProvider;
+
+class MediaManagerServiceProviderTest extends TestCase
+{
+    private $provider;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->provider = new MediaManagerServiceProvider(Container::getInstance());
+    }
+
+    /**
+     * Should register the service provider and make the media manager available
+     * on container.
+     *
+     * @return void
+     */
+    public function testRegisterWillResolveMediaManagerSingleton()
+    {
+        $this->setDisk('s3');
+        $this->provider->register();
+        
+        $container = Container::getInstance();
+        $this->assertInstanceOf(MediaManager::class, $container->make(MediaManager::class));
+    }
+
+    /**
+     * Should register the upload subscriber to events.
+     *
+     * @return void
+     */
+    public function testRegisterSubscriberUploadSubscriberFromConfiguration()
+    {
+        $this->setDisk('s3');
+        $this->provider->register();
+
+        $subscribers = Container::getInstance()['events']->getListeners(UploadSuccess::class);
+        
+        $this->assertInstanceOf(
+            Closure::class,
+            head($subscribers)
+        );
+    }
+
+    /**
+     * Should boot the lincable configuration.
+     *
+     * @return void
+     */
+    public function testBootWillRegisterTheConfigurationFile()
+    {
+        $this->provider->boot();
+        $config = __DIR__.'/../../config/lincable.php';
+        
+        $container = Container::getInstance();
+        $this->assertEquals(require $config, $container['config']['lincable']);
+    }
+}
