6aad8057fa6872b6afbb0bbde564baa8f24e8047
= <=>
2018-07-11 17:15:32 -0300

    Fix merge conflicts

--- src/Lincable/Providers/MediaManagerServiceProvider.php
+++ src/Lincable/Providers/MediaManagerServiceProvider.php
@@ -23,11 +23,7 @@ class MediaManagerServiceProvider extends ServiceProvider
     public function boot()
     {
         $configPath = __DIR__.'/../../../config/lincable.php';
-<<<<<<< HEAD
-
-=======
         
->>>>>>> 4c8a26f5a973bebf5b69c343119e2161a489f17b
         $this->publishes([
             $configPath => config_path('lincable.php'),
         ]);
@@ -45,11 +41,7 @@ class MediaManagerServiceProvider extends ServiceProvider
         $this->app->singleton(MediaManager::class, function ($app) {
             return new MediaManager($app, new UrlCompiler);
         });
-<<<<<<< HEAD
-
-=======
         
->>>>>>> 4c8a26f5a973bebf5b69c343119e2161a489f17b
         $this->app['events']->subscribe($this->app['config']['lincable.upload_subscriber']);
     }
 
